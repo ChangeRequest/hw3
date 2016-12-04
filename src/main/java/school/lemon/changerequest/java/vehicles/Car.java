12fb49ed4739218598b41890c82ecaefd9cf1e06
Yaroslav Pavlinskiy <yarbezl@gmail.com>
2016-12-05 01:24:43 +0200

    Task Vehicles done.

---
+++ src/main/java/school/lemon/changerequest/java/vehicles/Car.java
@@ -0,0 +1,92 @@
+package school.lemon.changerequest.java.vehicles;
+
+/**
+ * Created by Yaroslav Pavlinskiy on 02.12.2016.
+ */
+public class Car implements ICar {
+    private int manufacturedYears;
+    private String make;
+    private String model;
+    private int horespower;
+
+    public Car(int manufacturedYears, String make, String model, int horespower) {
+        this.manufacturedYears = manufacturedYears;
+        this.make = make;
+        this.model = model;
+        this.horespower = horespower;
+    }
+
+    @Override
+    public int getHorsepower() {
+        return this.horespower;
+    }
+
+    @Override
+    public void setHorsepower(int horsepower) {
+        this.horespower = horsepower;
+    }
+
+    @Override
+    public int getManufacturedYear() {
+        return this.manufacturedYears;
+    }
+
+    @Override
+    public void setManufacturedYear(int year) {
+        this.manufacturedYears = year;
+    }
+
+    @Override
+    public String getMake() {
+        return this.make;
+    }
+
+    @Override
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    @Override
+    public String getModel() {
+        return this.model;
+    }
+
+    @Override
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public String accelerate() {
+        return "fire pistons, turn wheels";
+    }
+
+    @Override
+    public String steerLeft() {
+        return "turn wheels left";
+    }
+
+    @Override
+    public String steerRight() {
+        return "turn wheels right";
+    }
+
+    @Override
+    public String toString() {
+        return "This car is a " + this.getManufacturedYear() + " " + this.getMake() + " " + this.getModel() + " with " + this.getHorsepower() + " hp.";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Boat)
+            obj = (Car) obj;
+        if (obj == null)
+            return false;
+        if (this == obj)
+            return true;
+        if (this.getHorsepower() + 10 != ((Car) obj).getHorsepower() || this.getHorsepower() - 10 != ((Car) obj).getHorsepower())
+            return true;
+        return false;
+    }
+
+}
